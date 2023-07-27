4ce10652d55bf9996fe9bdcf3eba2d14b44e80f3
FinoshkinPavel <turbososki@gmail.com>
2023-07-27 17:27:09 +0600

    add styles, add in storybook

--- src/components/TextField/TextField.stories.ts
+++ src/components/TextField/TextField.stories.ts
@@ -14,6 +14,22 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
-	args: {},
+export const Default: Story = {
+	args: {
+		label: "User name",
+	},
+};
+
+export const Error: Story = {
+	args: {
+		label: "User name",
+		errorMessage: "Error message",
+	},
+};
+
+export const Disabled: Story = {
+	args: {
+		label: "User name",
+		disabled: true,
+	},
 };
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,5 +1,29 @@
+import React from "react";
+import { Icon } from "./Icon";
 import s from "./TextField.module.scss";
+import icon from "./icon.svg";
 
-export const TextField = () => {
-	return <input type="text" className={s.textField} />;
+type TextFieldPropsType = {
+	label?: string;
+	errorMessage?: string;
+	disabled?: true;
+};
+
+export const TextField: React.FC<TextFieldPropsType> = (props) => {
+	const { disabled, errorMessage, label } = props;
+
+	return (
+		<div className={s.textFieldWrap}>
+			<div className={s.label}>{label}</div>
+			<input
+				type="text"
+				disabled={disabled}
+				placeholder="email.com"
+				className={`${s.textField} ${errorMessage && s.errorInput} ${
+					disabled && s.disabledInput
+				}`}
+			/>
+			{errorMessage && <div className={s.error}>{errorMessage}</div>}
+		</div>
+	);
 };
