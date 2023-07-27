213b41c71ea0b97f6632e7aa5224b593678f8088
FinoshkinPavel <turbososki@gmail.com>
2023-07-27 14:54:45 +0600

    add component TextField in the app and storybook

---
+++ src/components/TextField/TextField.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { TextField } from "./TextField";
+
+const meta = {
+	title: "Components/TextField",
+	component: TextField,
+	parameters: {
+		layout: "centered",
+	},
+	tags: ["autodocs"],
+} satisfies Meta<typeof TextField>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+	args: {},
+};
---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,5 @@
+import s from "./TextField.module.scss";
+
+export const TextField = () => {
+	return <input type="text" className={s.textField} />;
+};
