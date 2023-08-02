44e1cdc3b4619a247d371be51cf80cde5abf11b2
Elena1789 <elenadubovskaya8@gmail.com>
2023-08-02 18:16:04 +0300

    radix component was added

--- src/components/ui/selectbox/Selectbox.tsx
+++ src/components/ui/selectbox/Selectbox.tsx
@@ -1,30 +1,53 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import './selectbox.css';
+import * as Select from '@radix-ui/react-select';
 
-interface SelectProps { 
-    status?: 'default' | 'active' | 'hover' | 'focus' | 'disabled';   
+interface SelectProps {
+  status?: 'default' | 'active' | 'hover' | 'focus' | 'disabled';
 }
 
 export const Selectbox = ({
-    status,
+  status,
 }: SelectProps) => {
-    const mode = `storybook-select--${status}`;
+  const mode = `storybook-select--${status}`;
 
-    const [selectedOption, setSelectedOption] = useState<String>();
-    const selectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const value = event.target.value;
-        setSelectedOption(value);
-      };
+  const [selectedOption, setSelectedOption] = useState<String>();
+  const selectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setSelectedOption(value);
+  };
 
-    return (
-        <div className="selectbox" >
-            <label className='label'>Select-box</label>
-      <select onChange={selectChange} className={mode}>
-        <option className='optionline' value="selectbox">Select-box</option>
-        <option className='optionline' value="selectbox">Select-box</option>
-        <option className='optionline' value="selectbox">Select-box</option>
-      </select>
+  return (
+    <Select.Root>
+      <Select.Trigger className="selectbox">
+        <Select.Group>
+          <Select.Label className='label'>Select-box</Select.Label>
+        </Select.Group>
+        <Select.Group>
+          <Select.Label className={mode}><h1>Select-box</h1></Select.Label>
+        </Select.Group>
+      </Select.Trigger>
+
+      <Select.Portal >
+        <Select.Content>
+          <Select.Viewport>
+            <Select.Group className="options">
+              <Select.Item value="select-box" className="optionline">
+                <h1 className="line">Select-box</h1>
+              </Select.Item>
+              <Select.Item value="select-box" className="optionline">
+                <h1 className="line">Select-box</h1>
+              </Select.Item>
+              <Select.Item value="select-box" className="optionline">
+                <h1 className="line">Select-box</h1>
+              </Select.Item>
+            </Select.Group>
+          </Select.Viewport>
+        </Select.Content>
+      </Select.Portal>
+
       {selectedOption && <h2 >{selectedOption}</h2>}
-        </div>
-    );
+
+    </Select.Root>
+  );
 };
