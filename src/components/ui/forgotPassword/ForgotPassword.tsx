ff407142ec4194b4d7944d219f2f638a1094548b
czntrn <czenturijon@gmail.com>
2023-08-05 18:06:14 +0300

    forgot password init

---
+++ src/components/ui/forgotPassword/ForgotPassword.tsx
@@ -0,0 +1,16 @@
+import s from './forgotPassword.module.scss';
+import {Typography} from "src/components/ui/typography/typography";
+
+export const ForgotPassword = () => {
+    return (
+        <div
+            className={s.main}
+        >
+            <Typography
+
+            >
+                Forgot Password
+            </Typography>
+        </div>
+    )
+};
