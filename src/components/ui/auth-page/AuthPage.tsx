5229bfffb67d0264e5cb31134f9b0871d194e6c2
Nikita1025 <nekit1025l@gmail.com>
2023-08-06 15:02:17 +0300

    add: create invalid link page

--- src/components/ui/auth-page/AuthPage.tsx
+++ src/components/ui/auth-page/AuthPage.tsx
@@ -1,23 +1,24 @@
 import React, {FC} from 'react';
 import s from '@/src/components/ui/auth-page/AuthPage.module.scss';
 import {Button} from '@/src/components/ui/button';
+import {Typography} from "@/src/components/ui/typography/typography";
 
 type AuthPageType={
   title: string
   text: string
   nameButton: string
-  image?: string
   children?: React.ReactNode
 }
-export const AuthPage: FC<AuthPageType> = ({title, children, text, nameButton, image}) => {
+export const AuthPage: FC<AuthPageType> = ({title, children, text, nameButton}) => {
   return (
     <div className={s.container}>
       <div className={s.text_container}>
         <span className={s.title}>{title}</span>
         <span className={s.text}>{text}</span>
-        <Button variant="primary" className={s.button}>{nameButton}</Button>
+        <Button variant="primary" fullWidth={true} className={s.button}>
+          <Typography variant='bold14'>{nameButton}</Typography>
+        </Button>
       </div>
-      {/*<Image src={image} alt='image'/>*/}
       {children}
     </div>
     );
