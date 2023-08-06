b7379eabaa4eb5a01ff7feb21f4a21b323bbbc1a
Artyom <ptukhovAN@gmail.com>
2023-08-06 11:58:12 +0500

    fix: some errors in SB

--- src/pages/emailVerification/EmailVerification.tsx
+++ src/pages/emailVerification/EmailVerification.tsx
@@ -1,10 +1,13 @@
-import emailVerification from "@/src/assets/images/EmailVerification.svg";
-import {AuthPage} from "@/src/components/ui/auth-page/AuthPage";
+// import emailVerification from "@/src/assets/images/EmailVerification.svg";
+import {AuthPage} from '@/src/components/ui/auth-page/AuthPage';
+import TimeManagementImage from '@/src/assets/images/time-management-image';
+
 export const EmailVerification = () => {
-    return  <AuthPage title='Email verification link expired'
-                      text='Looks like the verification link has
+    return <AuthPage title="Email verification link expired"
+                     text="Looks like the verification link has
                       expired. Not to worry, we can send the
-                      link again'
-                      nameButton='Resend verification link'
-                      image={emailVerification}/>
+                      link again"
+                     nameButton="Resend verification link"
+      // image={emailVerification}/>
+    >{<TimeManagementImage/>}</AuthPage>
 };
