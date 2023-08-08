e0fe456608e48264291f141f93724b6c97b73ac4
FinoshkinPavel <turbososki@gmail.com>
2023-08-08 12:40:32 +0600

    fix: fix eye icon in pass textfield, fixed error message

--- src/components/ui/TextField/UIInput/UIInput.tsx
+++ src/components/ui/TextField/UIInput/UIInput.tsx
@@ -1,40 +1,48 @@
-import React, {ChangeEvent, ComponentProps, useState,} from 'react';
-import s from './UIInput.module.scss';
-import EyeIcon from '@/src/assets/icons/eye-icon';
-import EyeOffIcon from '@/src/assets/icons/eye-off-icon';
-import SearchIcon from '@/src/assets/icons/search-icon';
-import {Typography} from '@/src/components/ui/typography';
+import React, { ChangeEvent, ComponentProps, useState } from "react";
+import s from "./UIInput.module.scss";
+import EyeIcon from "@/src/assets/icons/eye-icon";
+import EyeOffIcon from "@/src/assets/icons/eye-off-icon";
+import SearchIcon from "@/src/assets/icons/search-icon";
+import { Typography } from "@/src/components/ui/typography";
 
 type UIInputPropsType = {
-	value?: string
+	value?: string;
 	label?: string;
 	errorMessage?: string;
 	onChangeText?: (value: string) => void;
-} & ComponentProps<'input'>;
+} & ComponentProps<"input">;
 
-export const UIInput = (
-	{type, disabled, onChangeText, errorMessage, label, placeholder, value, ...rest}: UIInputPropsType
-) => {
+export const UIInput: React.FC<UIInputPropsType> = (props) => {
+	const {
+		type,
+		disabled,
+		onChangeText,
+		errorMessage,
+		label,
+		placeholder,
+		value,
+		...rest
+	} = props;
 
-	const [showPass, setShowPass] = useState<'text' | 'password'>('password');
+	const [showPass, setShowPass] = useState<"text" | "password">("password");
 
 	const openClosePssHandler = () =>
-		showPass === 'password' ? setShowPass('text') : setShowPass('password');
+		showPass === "password" ? setShowPass("text") : setShowPass("password");
 
 	const onchangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
 		onChangeText && onChangeText(e.currentTarget.value);
 	};
 
-	const showErrorMess = errorMessage && errorMessage.length > 0
+	const showErrorMess = errorMessage && errorMessage.length > 0;
 
 	const _type =
-		type === 'text'
-			? 'text'
-			: type === 'password'
-				? showPass
-				: type === 'search'
-					? 'search'
-					: 'text';
+		type === "text"
+			? "text"
+			: type === "password"
+			? showPass
+			: type === "search"
+			? "search"
+			: "text";
 
 	return (
 		<div className={s.textFieldWrap}>
@@ -53,23 +61,22 @@ export const UIInput = (
 			{type === "password" &&
 				(showPass === "password" ? (
 					<EyeIcon
-						className={`${s.iconOpenEye} ${disabled && s.disabledIconEye}`}
+						className={`${s.iconEye} ${disabled && s.disabledIconEye}`}
 						onClick={openClosePssHandler}
 					/>
 				) : (
 					<EyeOffIcon
-						className={`${s.iconCloseEye} ${disabled && s.disabledIconEye}`}
+						className={`${s.iconEye} ${disabled && s.disabledIconEye}`}
 						onClick={openClosePssHandler}
 					/>
 				))}
 
-			{type === 'search' && <SearchIcon className={s.searchIcon}/>}
-			{showErrorMess &&
-          <Typography variant={'regular14'} color={'error'}>
-						{errorMessage}
-          </Typography>
-			}
+			{type === "search" && <SearchIcon className={s.searchIcon} />}
+			{showErrorMess && (
+				<Typography variant={"regular14"} className={s.errorWrap}>
+					{errorMessage}
+				</Typography>
+			)}
 		</div>
 	);
-}
-
+};
