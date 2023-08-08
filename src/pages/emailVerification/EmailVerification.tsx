7fe475e85bce13ae784d5b363ebb982553b2e20e
Nikita1025 <nekit1025l@gmail.com>
2023-08-08 20:47:14 +0300

    refactor: using AuthLayout pages

--- src/pages/emailVerification/EmailVerification.tsx
+++ src/pages/emailVerification/EmailVerification.tsx
@@ -1,11 +1,15 @@
 import {AuthPage} from '@/src/components/ui/auth-page/AuthPage';
 import TimeManagementImage from '@/src/assets/images/time-management-image';
+import { AuthLayout } from '@/src/components/Layout/AuthLayout';
 
 export const EmailVerification = () => {
-    return <AuthPage title="Email verification link expired"
-                     text="Looks like the verification link has
+    return <AuthLayout>
+        <AuthPage title="Email verification link expired"
+                  text="Looks like the verification link has
                       expired. Not to worry, we can send the
                       link again"
-                     nameButton="Resend verification link"
-    >{<TimeManagementImage/>}</AuthPage>
+                  nameButton="Resend verification link"
+        >{<TimeManagementImage/>}</AuthPage>
+    </AuthLayout>
+
 };
