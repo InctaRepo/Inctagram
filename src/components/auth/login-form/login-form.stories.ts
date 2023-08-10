83b3a57faa6b239565755ba175c956a0b959ac0c
Nikita1025 <nekit1025l@gmail.com>
2023-08-10 19:45:37 +0300

    add: add routing

--- src/components/auth/login-form/login-form.stories.ts
+++ src/components/auth/login-form/login-form.stories.ts
@@ -3,17 +3,17 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { LogInform } from "./LogIn-form";
 
 const meta = {
-	title: "Auth/LogInForm",
-	component: LogInform,
-	parameters: {
-		layout: "centered",
-	},
-	tags: ["autodocs"],
+  title: "Auth/LogInForm",
+  component: LogInform,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
 } satisfies Meta<typeof LogInform>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-	args: {},
+  args: {},
 };
--- src/pages/auth/sign-up/index.tsx
+++ src/pages/auth/sign-up/index.tsx
@@ -1,29 +1,36 @@
-import {RegisterForm, RegisterFormType} from '@/src/components/auth/register-form';
-import {NextPageWithLayout} from '@/src/pages/_app';
-import {getAuthLayout} from '@/src/components/Layout/AuthLayout/AuthLayout';
-import {useRegistrationMutation} from '@/src/api/authApi/authApi';
-import {Typography} from '@/src/components/ui/typography';
+import {
+  RegisterForm,
+  RegisterFormType,
+} from "@/src/components/auth/register-form";
+import { NextPageWithLayout } from "@/src/pages/_app";
+import { getAuthLayout } from "@/src/components/Layout/AuthLayout/AuthLayout";
+import { useRegistrationMutation } from "@/src/api/authApi/authApi";
+import { Typography } from "@/src/components/ui/typography";
 
 const SignUpPage: NextPageWithLayout = () => {
+  const [register, { isError, isLoading, error, data, isSuccess }] =
+    useRegistrationMutation();
 
-	const [register, {isError, isLoading, error, data, isSuccess}] = useRegistrationMutation()
+  const submit = (data: RegisterFormType) => {
+    register(data);
+  };
 
-	const submit = (data: RegisterFormType) => {
-		register(data)
-	}
+  if (isLoading) {
+    return <div>Loading...</div>;
+  } else
+    return (
+      <>
+        {/*@ts-ignore*/}
+        {isError && (
+          <Typography color={"error"}>
+            {error?.data.message[0].message}
+          </Typography>
+        )}
+        <RegisterForm onSubmitHandler={submit} />
+      </>
+      //TODO linkpath to sign in
+    );
+};
 
-	if (isLoading) {
-		return <div>Loading...</div>
-	} else
-		return (
-			<>
-				{/*@ts-ignore*/}
-				{isError && <Typography color={'error'}>{error?.data.message[0].message}</Typography>}
-				<RegisterForm linkPath={'#'} onSubmitHandler={submit}/>
-			</>
-			//TODO linkpath to sign in
-		)
-}
-
-SignUpPage.getLayout = getAuthLayout
-export default SignUpPage
+SignUpPage.getLayout = getAuthLayout;
+export default SignUpPage;
