095d6cbc1b9c44ac35ac851475eebe8e8b0dab89
Elena1789 <elenadubovskaya8@gmail.com>
2023-08-16 09:06:00 +0300

    init: create and sent new password to server

--- src/assets/api/auth-api.ts
+++ src/assets/api/auth-api.ts
@@ -6,12 +6,23 @@ export const authAPI = {
   },
 }
 
+export const newPasswordAPI = {
+  createNewPassword(formData: NewPasswordArgsType) {
+    return instance.post('auth/new-password', formData)
+  },
+}
+
 //TYPES ====================================================================================
 export type RegisterArgsType = {
   username: string
   email: string
   password: string
   passwordConfirm: string
+}
+
+export type NewPasswordArgsType = {
+  newPassword: string
+  recoveryCode: '3fa85f64-5717-4562-b3fc-2c963f66afa6'
 }
 
 export type ResponseType<D = {}> = {
