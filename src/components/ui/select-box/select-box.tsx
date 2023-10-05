3dbbd3a9d0b0d098c78edcbcede11fcdf54de84a
Elena1789 <elenadubovskaya8@gmail.com>
2023-10-05 14:36:20 +0200

    feature: scrollbar was added to selectbox

--- src/components/ui/select-box/select-box.tsx
+++ src/components/ui/select-box/select-box.tsx
@@ -1,5 +1,6 @@
 import React, { FC, ReactElement, ReactNode, useState } from 'react'
 
+import * as ScrollArea from '@radix-ui/react-scroll-area'
 import * as Select from '@radix-ui/react-select'
 
 import styles from './selectbox.module.scss'
@@ -46,6 +47,10 @@ export const SelectBox: FC<SelectProps> = ({
     selectContent: styles.selectContent,
     line: styles.line,
     value: styles.value,
+    scrollAreaRoot: styles.scrollAreaRoot,
+    scrollAreaViewport: styles.scrollAreaViewport,
+    scrollAreaScrollbar: styles.scrollAreaScrollbar,
+    scrollAreaThumb: styles.scrollAreaThumb,
   }
   const onChangeHandler = (newValue: string) => {
     setValue(newValue)
@@ -84,14 +89,21 @@ export const SelectBox: FC<SelectProps> = ({
 
       <Select.Portal>
         <Select.Content position={'popper'} className={s.selectContent}>
-          <Select.Viewport>
-            {options?.map((el, idx) => (
-              <Select.Item value={el.value.toString()} key={idx} className={s.line}>
-                {el.image}
-                <Select.ItemText>{el.value}</Select.ItemText>
-              </Select.Item>
-            ))}
-          </Select.Viewport>
+          <ScrollArea.Root className={s.scrollAreaRoot} type="auto">
+            <Select.Viewport asChild>
+              <ScrollArea.Viewport className={s.scrollAreaViewport}>
+                {options?.map((el, idx) => (
+                  <Select.Item value={el.value.toString()} key={idx} className={s.line}>
+                    {el.image}
+                    <Select.ItemText>{el.value}</Select.ItemText>
+                  </Select.Item>
+                ))}
+              </ScrollArea.Viewport>
+            </Select.Viewport>
+            <ScrollArea.Scrollbar className={s.scrollAreaScrollbar} orientation="vertical">
+              <ScrollArea.Thumb className={s.scrollAreaThumb} />
+            </ScrollArea.Scrollbar>
+          </ScrollArea.Root>
         </Select.Content>
       </Select.Portal>
     </Select.Root>
