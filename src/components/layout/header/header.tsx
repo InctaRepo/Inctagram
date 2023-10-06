a7ca615ba80ffb3032a5a2e608e96bfaeceaca3d
Sargis Adamyan <adamian.sargis@gmail.com>
2023-10-07 02:21:30 +0300

    init(auth): Implement multi-factor authentication (MFA) support

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,14 +1,18 @@
+'use client'
 import React from 'react'
 
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 import FlagRussiaIcon from '@/src/assets/icons/flag-russia-icon'
 import FlagUKIcon from '@/src/assets/icons/flag-UK-icon'
 import MaskIcon from '@/src/assets/icons/mask-icon'
 import { Typography } from '@/src/components/ui/typography'
+import { useAppSelector } from '@/src/services'
+import { authIsAuthSelector } from '@/src/services/auth/auth-selectors'
+import { RouteNames } from 'src/common/constants/route-names'
 import s from 'src/components/layout/header/header.module.scss'
 import { OptionsType, SelectBox } from 'src/components/ui/select-box'
-import Link from 'next/link'
 
 export const Header = () => {
   const { push, pathname, query, asPath, locale } = useRouter()
@@ -23,13 +27,23 @@ export const Header = () => {
       push({ pathname, query }, asPath, { locale })
     }
   }
+  const IsAuth = useAppSelector(authIsAuthSelector)
+
+  function navigateToAuth() {
+    if (IsAuth) {
+      push(RouteNames.MY_PROFILE)
+    } else {
+      push(RouteNames.SIGN_IN)
+    }
+  }
 
   return (
     <div className={s.container}>
       <div className={s.content}>
         <Typography variant="large" className={s.text}>
-          <Link className={s.incta} href={"/auth/sign-in"}>Inсtagram</Link>
-
+          <Link className={s.incta} href={RouteNames.SIGN_IN} onClick={navigateToAuth}>
+            Inсtagram
+          </Link>
         </Typography>
         <div className={s.options_container}>
           <MaskIcon />
