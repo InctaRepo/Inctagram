347e950c2e98977f0347213b8c2a53b27ec3b90d
Elena1789 <elenadubovskaya8@gmail.com>
2023-10-10 10:34:48 +0200

    feature: data picker validation and sent ava to backend was added

--- src/services/base-api.ts
+++ src/services/base-api.ts
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-export const baseUrl =
-  'https://ea19-193-200-201-228.ngrok-free.app/api/v1' /*'https://inctagram.space/api/v1/'*/
+export const baseUrl = 'https://inctagram.space/api/v1/'
 
 export const baseApi = createApi({
   reducerPath: 'baseApi',
