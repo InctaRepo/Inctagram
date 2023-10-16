71b0260279e7b744362ad3af616c7225e9dbaea6
Sargis Adamyan <adamian.sargis@gmail.com>
2023-10-16 15:45:18 +0300

    init(auth): replace Link with useRouter

--- src/components/auth/terms-privacy/terms-privacy-page/terms-service.tsx
+++ src/components/auth/terms-privacy/terms-privacy-page/terms-service.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import { Typography } from '../../../ui/typography'
 
@@ -13,21 +14,29 @@ import { useTranslate } from '@/src/assets/hooks'
 import ImageArrow from '@/src/assets/images/rightArrow.png'
 import { RouteNames } from '@/src/common/constants/route-names'
 import { Header } from '@/src/components/layout/header/header'
+import { Button } from '@/src/components/ui/button'
 
 export const Terms = () => {
   const { t } = useTranslate()
+  const router = useRouter()
 
   return (
     <div>
       <Header />
       <div className={s.main}>
+        {/*RouteNames.SIGN_IN*/}
         <Typography variant={'regular14'} className={s.SignUp}>
-          <Link className={s.link} href={RouteNames.SIGN_IN}>
+          <Button
+            as={'a'}
+            className={s.btn}
+            variant={'text'}
+            onClick={() => router.push(RouteNames.SIGN_IN)}
+          >
             <span className={s.arrow}>
               <Image className={s.img} src={ImageArrow} alt="arrow" />
               {t.auth.BackToSignUp}
             </span>
-          </Link>
+          </Button>
         </Typography>
 
         <div className={s.Text}>
