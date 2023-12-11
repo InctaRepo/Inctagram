3234d7c5fdb6d1b9ac491afb237fa92ae1da06e3
Chernik Maksim <5059585@mail.ru>
2023-12-11 14:38:05 +0300

    fix: refactor fsd

--- src/pages/profile/[id]/post/[postId].tsx
+++ src/pages/profile/[id]/post/[postId].tsx
@@ -7,13 +7,12 @@ import { getProfile } from '@/src/features/profile/service'
 import { NextPageWithLayout } from '@/src/shared/service/nextPageWithLayout'
 import { wrapper } from '@/src/store'
 import { getAuthLayout } from '@/src/widgets/layout/authLayout'
-//http://localhost:3000/profile/post/98bd128d-56e1-49be-a201-7aecc4beb748
+//http://localhost:3000/profile/d8d525f5-8d47-46f2-8b27-d7488ea9e40e/post/f5fefcf3-0d41-49a0-9afe-babaf0342a4c
 
 export const getServerSideProps = wrapper.getServerSideProps(store => async context => {
   const postId = context.query?.postId as string
   const id = context.query?.id as string
 
-  console.log(context, postId, id, '22222222222222222222222')
   store.dispatch(getUserPost.initiate(postId))
   store.dispatch(getProfile.initiate(id))
   store.dispatch(getUserPosts.initiate(id))
