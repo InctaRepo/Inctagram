5b6c0d2c55f60943c1858055742e45e79efcdb01
Chernik Maksim <5059585@mail.ru>
2023-12-16 06:03:19 +0300

    refactor: scroll

--- src/features/profile/ui/Profile.tsx
+++ src/features/profile/ui/Profile.tsx
@@ -4,11 +4,11 @@ import { useRouter } from 'next/router'
 
 import { ProfileInfo } from '@/src/entities/profile/profileInfo'
 // eslint-disable-next-line @conarti/feature-sliced/layers-slices
-import { Posts, useGetUserPostsQuery } from '@/src/features/posts'
+import { Posts } from '@/src/features/posts'
 import { useGetProfileQuery } from '@/src/features/profile/service'
 import { RouteNames } from '@/src/shared/const/routeNames'
 import { getIsAuth } from '@/src/shared/hoc'
-import { useAppSelector, useInfiniteScroll } from '@/src/shared/hooks'
+import { useAppSelector } from '@/src/shared/hooks'
 import { Sidebar } from '@/src/shared/sidebar'
 import { Loader } from '@/src/shared/ui/loader'
 import s from 'src/features/profile/ui/profile.module.scss'
@@ -18,50 +18,32 @@ type Props = {
   variant?: string
   postId?: string
 }
+//TODO нужен ли variant?
 
 export const Profile = ({ id, postId, variant }: Props) => {
   const isAuth = useAppSelector(getIsAuth)
   const router = useRouter()
-
-  const { data, refetch, isSuccess } = useGetProfileQuery(id)
-
-  const { data: posts } = useGetUserPostsQuery(data?.data.userId)
-
-  if (!posts) {
-    return null
-  }
-  const { isLoading, loadMoreCallback, hasDynamicPosts, dynamicPosts, isLastPage } =
-    useInfiniteScroll(posts.data.items)
+  const { data, refetch, isSuccess, isLoading } = useGetProfileQuery(id)
 
   useEffect(() => {
     if (data?.resultCode === 0) {
       refetch()
     }
   }, [])
-
   if (isSuccess && data?.resultCode === 5 && isAuth) {
     router.push(RouteNames.PROFILE_SETTINGS)
 
     return <Loader />
   }
-  // if (isLoading) return <Loader />
+  if (isLoading) return <Loader />
 
   return (
     <div className={s.container}>
       {isAuth && <Sidebar />}
       <div className={isAuth ? s.containerInfo : s.containerInfoPublic}>
         <ProfileInfo userData={data?.data} />
-        <Posts
-          userData={data?.data}
-          postId={postId}
-          variant={variant}
-          posts={hasDynamicPosts ? dynamicPosts : posts?.data.items}
-          isLoading={isLoading}
-          loadMoreCallback={loadMoreCallback}
-          isLastPage={isLastPage}
-        />
+        <Posts userData={data?.data} postId={postId} variant={variant} userId={id} />
       </div>
     </div>
   )
 }
-//TODO нужен ли variant?
