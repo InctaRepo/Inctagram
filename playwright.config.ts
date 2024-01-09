3bf1e884b25f68c8da244bfc3a033645a6c9c02d
Chernik Maksim <5059585@mail.ru>
2024-01-09 16:52:51 +0300

    fix: test

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'http://localhost:3000',
     locale: 'en-GB',
     timezoneId: 'Europe/Paris',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
@@ -63,8 +63,8 @@ export default defineConfig({
     command: 'next dev',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
-    // stdout: 'ignore',
-    // stderr: 'pipe',
+    stdout: 'ignore',
+    stderr: 'pipe',
     timeout: 120 * 1000,
   },
 })
--- tests/signIn.spec.ts
+++ tests/signIn.spec.ts
@@ -1,89 +1,54 @@
-import { errors, test } from '@playwright/test'
-// test.describe.configure({ retries: 1, timeout: 40_000 })
-// test('has title', async ({ page }) => {
-//   await page.goto('https://playwright.dev/')
-//
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/Playwright/)
-// })
-//
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/')
-//
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click()
-//
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible()
-// })
+import { test } from '@playwright/test'
 
 test('signIn', async ({ page }) => {
-  try {
-    await page.goto('http://localhost:3000/en')
-    await page.goto('http://localhost:3000/home')
-    await page.getByRole('button', { name: 'Sign In' }).click()
-    await page.locator('input[name="email"]').click()
-    await page.locator('input[name="email"]').fill('chernik5059585@gmail.com')
-    await page.locator('input[name="password"]').click()
-    await page.locator('input[name="password"]').fill('1qaz@WSX')
-    await page.getByRole('button', { name: 'Sign In' }).click()
-    await page.getByRole('link', { name: 'My Profile' }).click()
-    await page.waitForTimeout(5000)
-  } catch (error) {
-    if (error instanceof errors.TimeoutError) console.log('Timeout!')
-  }
+  await page.goto('http://localhost:3000')
+  await page.goto('http://localhost:3000/home')
+  await page.getByRole('button', { name: 'Sign In' }).click()
+  await page.locator('input[name="email"]').click()
+  await page.locator('input[name="email"]').fill('chernik5059585@gmail.com')
+  await page.locator('input[name="password"]').click()
+  await page.locator('input[name="password"]').fill('1qaz@WSX')
+  await page.getByRole('button', { name: 'Sign In' }).click()
+  await page.getByRole('link', { name: 'My Profile' }).click()
 })
 
 test('test', async ({ page }) => {
-  try {
-    await page.goto('http://localhost:3000/en')
-    await page.goto('http://localhost:3000/home')
-    await page.getByRole('button', { name: 'Sign In' }).click()
-    await page.locator('input[name="email"]').click()
-    await page.locator('input[name="email"]').fill('chernik5059585@gmail.com')
-    await page.locator('input[name="password"]').click()
-    await page.locator('input[name="password"]').fill('1qaz@WSX')
-    await page.getByRole('button', { name: 'Sign In' }).click()
-    await page.getByRole('link', { name: 'My Profile' }).click()
-    await page.locator('.showPostModal_postImage__cJ5FK > img').first().click()
-    await page.locator('.editModal_IconButton__r0PgZ > svg').click()
-    await page.getByRole('button', { name: 'Log Out' }).click()
-    await page.getByRole('button', { name: 'Yes' }).click()
-    await page.waitForTimeout(5000)
-  } catch (error) {
-    if (error instanceof errors.TimeoutError) console.log('Timeout!')
-  }
+  await page.goto('http://localhost:3000')
+  await page.goto('http://localhost:3000/home')
+  await page.getByRole('button', { name: 'Sign In' }).click()
+  await page.locator('input[name="email"]').click()
+  await page.locator('input[name="email"]').fill('chernik5059585@gmail.com')
+  await page.locator('input[name="password"]').click()
+  await page.locator('input[name="password"]').fill('1qaz@WSX')
+  await page.getByRole('button', { name: 'Sign In' }).click()
+  await page.getByRole('link', { name: 'My Profile' }).click()
+  await page.locator('.showPostModal_postImage__cJ5FK > img').first().click()
+  await page.locator('.editModal_IconButton__r0PgZ > svg').click()
+  await page.getByRole('button', { name: 'Log Out' }).click()
+  await page.getByRole('button', { name: 'Yes' }).click()
 })
 
 test('disc', async ({ page }) => {
-  try {
-    await page.goto('http://localhost:3000/en')
-    await page.goto('http://localhost:3000/home')
-    await page.getByRole('button', { name: 'Sign In' }).click()
-    await page.locator('input[name="email"]').click()
-    await page.locator('input[name="email"]').fill('chernik5059585@gmail.com')
-    await page.locator('input[name="password"]').click()
-    await page.locator('input[name="password"]').fill('1qaz@WSX')
-    await page.getByRole('button', { name: 'Sign In' }).click()
-    await page.getByRole('link', { name: 'My Profile' }).click()
-    await page.locator('.showPostModal_postImage__cJ5FK > img').first().click()
-    await page.locator('.postMenu_blue__nnUAa').click()
-    await page.getByText('Edit Post').click()
-    await page.getByRole('textbox').click()
-    await page.waitForTimeout(5000)
-    await page.getByRole('textbox').fill('eeeee')
-    await page.getByRole('button', { name: 'Save changes' }).click()
-    await page.waitForTimeout(500)
-    await page.locator('.postMenu_blue__nnUAa').click()
-    await page.waitForTimeout(500)
-    await page.getByText('Edit Post').click()
-    await page.getByLabel('Edit Post').getByText('eeeee').click()
-    await page.getByLabel('Edit Post').getByText('eeeee').fill('')
-    await page.getByRole('button', { name: 'Save changes' }).click()
-    await page.locator('.editModal_IconButton__r0PgZ > svg').click()
-    await page.locator('div:nth-child(2) > .showPostModal_postImage__cJ5FK > img').click()
-    await page.waitForTimeout(5000)
-  } catch (error) {
-    if (error instanceof errors.TimeoutError) console.log('Timeout!')
-  }
+  await page.goto('http://localhost:3000')
+  await page.goto('http://localhost:3000/home')
+  await page.getByRole('button', { name: 'Sign In' }).click()
+  await page.locator('input[name="email"]').click()
+  await page.locator('input[name="email"]').fill('chernik5059585@gmail.com')
+  await page.locator('input[name="password"]').click()
+  await page.locator('input[name="password"]').fill('1qaz@WSX')
+  await page.getByRole('button', { name: 'Sign In' }).click()
+  await page.getByRole('link', { name: 'My Profile' }).click()
+  await page.locator('.showPostModal_postImage__cJ5FK > img').first().click()
+  await page.locator('.postMenu_blue__nnUAa').click()
+  await page.getByText('Edit Post').click()
+  await page.getByRole('textbox').click()
+  await page.getByRole('textbox').fill('eeeee', { timeout: 500 })
+  await page.getByRole('button', { name: 'Save changes' }).click({ timeout: 500 })
+  await page.locator('.postMenu_blue__nnUAa').click()
+  await page.getByText('Edit Post').click()
+  await page.getByLabel('Edit Post').getByText('eeeee').click()
+  await page.getByLabel('Edit Post').getByText('eeeee').fill('')
+  await page.getByRole('button', { name: 'Save changes' }).click()
+  await page.locator('.editModal_IconButton__r0PgZ > svg').click()
+  await page.locator('div:nth-child(2) > .showPostModal_postImage__cJ5FK > img').click()
 })
