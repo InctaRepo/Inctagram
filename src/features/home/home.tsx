44a878eae7955ffff774ab2d443b450b732e416c
ilich01 <sidea.ilyaderkach@gmail.com>
2024-01-24 15:46:52 +0700

    rename

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
 
-// eslint-disable-next-line @conarti/feature-sliced/layers-slices
-import { Posts } from '../posts'
-
 import { Alerts } from './Alerts/Alerts'
 import s from './home.module.scss'
 import { PublicPost } from './PublicPost/PublicPost'
@@ -10,7 +7,6 @@ import { PublicPost } from './PublicPost/PublicPost'
 import { getIsAuth } from '@/src/shared/hoc'
 import { useAppSelector } from '@/src/shared/hooks'
 import { Sidebar } from '@/src/shared/sidebar'
-import { Typography } from '@/src/shared/ui/typography'
 
 export const Home = () => {
   const isAuth = useAppSelector(getIsAuth)
