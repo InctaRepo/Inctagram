f3eaa86483ec46796a5cde02dbd7b40f7b5cc3a6
Sargis Adamyan <adamian.sargis@gmail.com>
2024-02-13 17:46:55 +0300

    ubdate

--- src/entities/profile/settings/myPayment/ui/MyPaymentDynamic.tsx
+++ src/entities/profile/settings/myPayment/ui/MyPaymentDynamic.tsx
@@ -2,6 +2,9 @@ import dynamic from 'next/dynamic'
 
 import { Loader } from '@/ui/loader'
 
-export const MyPaymentDynamic = dynamic(() => import('./examples').then(mod => mod.Main), {
-  loading: () => <Loader />,
-})
+export const MyPaymentDynamic = dynamic(
+  () => import('./examples/MyPayment').then(mod => mod.Main),
+  {
+    loading: () => <Loader />,
+  }
+)
