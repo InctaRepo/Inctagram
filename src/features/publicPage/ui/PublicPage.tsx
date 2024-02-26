3766650d140c414b65e34bc8625ff313e195964f
Chernik Maksim <5059585@mail.ru>
2024-02-26 15:01:52 +0300

    fix: PublicPage

--- src/features/publicPage/ui/PublicPage.tsx
+++ src/features/publicPage/ui/PublicPage.tsx
@@ -3,20 +3,12 @@ import React from 'react'
 import { Alerts } from '@/features/publicPage/ui/alerts'
 import s from '@/features/publicPage/ui/publicPage.module.scss'
 import { PublicPost } from '@/features/publicPage/ui/publicPost'
-import { getIsAuth } from '@/shared/hoc'
-import { useAppSelector } from '@/shared/hooks'
-import { Sidebar } from '@/shared/sidebar'
 
 export const PublicPage = () => {
-  const isAuth = useAppSelector(getIsAuth)
-
   return (
     <div className={s.container}>
-      {isAuth && <Sidebar />}
-      <div className={isAuth ? s.containerInfo : s.containerInfoPublic}>
-        <Alerts />
-        <PublicPost />
-      </div>
+      <Alerts />
+      <PublicPost />
     </div>
   )
 }
