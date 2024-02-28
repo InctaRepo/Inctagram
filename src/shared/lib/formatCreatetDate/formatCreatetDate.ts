cea5d1c8e827155e2733ced25ec003ffa612d75d
Chernik Maksim <5059585@mail.ru>
2024-02-28 08:46:37 +0300

    fix: TimeAgoAdd

--- src/shared/lib/formatCreatetDate/formatCreatetDate.ts
+++ src/shared/lib/formatCreatetDate/formatCreatetDate.ts
@@ -1,21 +1,14 @@
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+import ru from 'javascript-time-ago/locale/ru'
+import { useRouter } from 'next/router'
+
 export const formatPostCreatedAt = (createdAt: string): string => {
-  const now = new Date()
+  TimeAgo.addLocale(en)
+  TimeAgo.addLocale(ru)
+  const { locale } = useRouter()
+  const timeAgo = new TimeAgo(locale!)
   const postDate = new Date(createdAt)
-  const differenceInMinutes = Math.floor((now.getTime() - postDate.getTime()) / (1000 * 60))
 
-  if (differenceInMinutes < 2) {
-    return `${differenceInMinutes} min ago`
-  } else if (differenceInMinutes < 24 * 60) {
-    const differenceInHours = Math.floor(differenceInMinutes / 60)
-
-    return `${differenceInHours} hours ago`
-  } else if (differenceInMinutes < 30 * 24 * 60) {
-    const differenceInDays = Math.floor(differenceInMinutes / (24 * 60))
-
-    return `${differenceInDays} days ago`
-  } else {
-    const differenceInMonths = Math.floor(differenceInMinutes / (30 * 24 * 60))
-
-    return `${differenceInMonths} month ago`
-  }
+  return timeAgo.format(postDate.getTime(), 'round-minute')
 }
