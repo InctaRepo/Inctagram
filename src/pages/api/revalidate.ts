c557ca78efc08676cc6d6905a1305c17e2dfb8f1
Chernik Maksim <5059585@mail.ru>
2024-02-28 13:16:50 +0300

    fix: revalidate

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<any> {
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+  if (req.query.secret !== 'ttt') {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
