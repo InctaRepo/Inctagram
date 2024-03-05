ccf11546681989893a626537981acc9561317fab
Chernik Maksim <5059585@mail.ru>
2024-03-05 10:27:36 +0300

    fix: invalid code expire

--- src/features/auth/emailVerification/ui/EmailVerification.tsx
+++ src/features/auth/emailVerification/ui/EmailVerification.tsx
@@ -1,16 +1,10 @@
-import { AuthPage } from '@/entities/auth/authPage'
-import EmailConfirmed from '@/public/icon/emailComfirmedIcon.svg'
+import React from 'react'
+
+import { useEmailRecoveryMutation } from '@/features/auth/emailVerification/service/emailRecovery'
+import { Recovery } from '@/features/auth/recovery'
 
 export const EmailVerification = () => {
-  return (
-    <AuthPage
-      title="Email verification link expired"
-      text="Looks like the verification link has
-                      expired. Not to worry, we can send the
-                      link again"
-      nameButton="Resend verification link"
-    >
-      <EmailConfirmed />
-    </AuthPage>
-  )
+  const [emailRecovery] = useEmailRecoveryMutation()
+
+  return <Recovery recoveryMutation={emailRecovery} type={'email'} />
 }
--- src/features/auth/passwordRecovery/ui/PasswordRecovery.tsx
+++ src/features/auth/passwordRecovery/ui/PasswordRecovery.tsx
@@ -1,47 +1,8 @@
-import React, { useState } from 'react'
-
 import { usePasswordRecoveryMutation } from '@/features/auth/passwordRecovery/service/passwordRecovery'
-import { PasswordRecoveryParams } from '@/features/auth/passwordRecovery/service/types/passwordRecoveryParams'
-import { PasswordRecoveryForm } from '@/features/auth/passwordRecovery/ui/passwordRecoveryForm'
-import s from '@/features/auth/passwordRecovery/ui/passwordRecoveryForm/passwordRecoveryForm.module.scss'
-import { useTranslate } from '@/shared/hooks'
-import { Button } from '@/ui/button/Button'
-import { Modal } from '@/ui/modal'
-import { Typography } from '@/ui/typography'
+import { Recovery } from '@/features/auth/recovery'
 
 export const PasswordRecovery = () => {
   const [passwordRecovery] = usePasswordRecoveryMutation()
-  const { t } = useTranslate()
-  const [email, setEmail] = useState('')
-  const [openModal, setOpenModal] = useState(false)
-  const modalHandler = () => {
-    setOpenModal(!openModal)
-  }
 
-  const submit = (data: PasswordRecoveryParams) => {
-    passwordRecovery(data)
-    setEmail(data.email.length > 30 ? data.email.slice(0, 30) + '...' : data.email)
-  }
-
-  return (
-    <>
-      <PasswordRecoveryForm onSubmitHandler={submit} modalHandler={modalHandler} />
-      <Modal
-        className={s.modal}
-        modalWidth={'sm'}
-        open={openModal}
-        title={t.auth.emailSent}
-        onAction={modalHandler}
-        onCancel={modalHandler}
-        onClose={modalHandler}
-      >
-        <Typography variant="regular16">{t.auth.emailConfirm(email)}</Typography>
-        <div className={s.buttonOkWrapper}>
-          <Button className={s.buttonOk} onClick={modalHandler} type="button">
-            {t.auth.ok}
-          </Button>
-        </div>
-      </Modal>
-    </>
-  )
+  return <Recovery recoveryMutation={passwordRecovery} type={'password'} />
 }
