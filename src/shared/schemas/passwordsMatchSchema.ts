0514edff8cf64ab866daad970038883feb4da285
Chernik Maksim <5059585@mail.ru>
2024-04-05 11:28:03 +0300

    refactor: Translate

--- src/shared/schemas/passwordsMatchSchema.ts
+++ src/shared/schemas/passwordsMatchSchema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
-import { LocaleType } from '@/public/locales/en'
+import { LocaleType } from '@/public/locales/ru'
 
 export function passwordsMatchSchema(t: LocaleType) {
   return z
